39a34c4b2840042950c1b8b4ae5d301c5f14818c
Lucas <89718602+luscra0@users.noreply.github.com>
2021-08-29 21:58:01 -0300

    draw_next_layer() now able to draw other polygons

--- Experiments/turtle_shape_thingy.py
+++ Experiments/turtle_shape_thingy.py
@@ -2,13 +2,12 @@ import turtle
 import math
 import random
 
-def draw_square(sides):
-    t1.pencolor(0, 0, 0)
+def draw_next_layer(sides):
     t1.fillcolor(random.randint(0,255),random.randint(0,255),random.randint(0,255))
     t1.begin_fill()
     for side in sides:
         t1.forward(side)
-        t1.left(90)
+        t1.left(360/len(sides))
     t1.end_fill()
 
 def calculate_next_layer(old_sides, internal_l1):
@@ -21,27 +20,20 @@ def calculate_next_layer(old_sides, internal_l1):
     new_sides.append(triangles[-1][1])
     return new_sides, internal_l1
 
-def calculate_triangle(angle, opposite=None, adjacent=None, hypotenuse=None):
-    sin = math.sin(math.radians(angle))
+def calculate_triangle(angle, adjacent=None, hypotenuse=None):
     cos = math.cos(math.radians(angle))
-    if opposite:
-        c = opposite
-        a = c/sin
-        b = math.sqrt(a**2 - c**2)
-        return a, b, c
-    elif adjacent:
+    if adjacent:
         b = adjacent
         a = b/cos
-        c = math.sqrt(a**2 - b**2)
-        return a, b, c
     elif hypotenuse:
         a = hypotenuse
-        c = a*sin
-        b = math.sqrt(a**2 - c**2)
-        return a, b, c
+        b = a*cos
+    c = math.sqrt(a**2 - b**2)
+    return a, b, c
 
 size_of_screen = 600
-sides = [size_of_screen-40, size_of_screen-40, size_of_screen-40, size_of_screen-40]
+num_sides = 4
+sides = [size_of_screen-40 for i in range(num_sides)]
 internal_l1 = size_of_screen-40
 angle = 5
 
@@ -53,12 +45,14 @@ screen.setup(width=size_of_screen, height=size_of_screen)
 screen.colormode(255)
 
 t1.speed(8)
+t1.pencolor(0, 0, 0)
 t1.pu()
 t1.setpos((-(size_of_screen)/2)+20,(-(size_of_screen)/2)+20)
 t1.pd()
 
 while sides[3] > 10:
-    draw_square(sides)
+    draw_next_layer(sides)
     sides, internal_l1 = calculate_next_layer(sides, internal_l1)
     t1.left(angle)
+
 screen.exitonclick()
