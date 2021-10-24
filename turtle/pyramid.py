2d4af1f4e277959acec21d0200fc8b631473253d
Lucas <89718602+luscra0@users.noreply.github.com>
2021-10-23 21:43:29 -0300

    New Axis

--- turtle/pyramid.py
+++ turtle/pyramid.py
@@ -1,24 +1,28 @@
 import turtle
 import math
 from time import sleep
+from typing import AsyncGenerator
 
-def ellipse(tur, r1, r2, angle=360):
-    pyramid_base_points = []
-    for a in range(angle+1):
-        x = pyramid_pos[0] + (r1 * math.cos(math.radians(a)))
-        y = pyramid_pos[1] + (r2 * math.sin(math.radians(a)))
-        if a in pyramid_base_angles:
-            pyramid_base_points.append((x, y))
-        jump(tur, (x, y))
-    return pyramid_base_points
+def calculate_points(pos, r1, r2, angles):
+    points = []
+    for a in angles:
+        x = pos[0] + (r1 * math.cos(math.radians(a)))
+        y = pos[1] + (r2* math.sin(math.radians(a)))
+        points.append((x, y))
+    return points
 
-def draw_pyramid(tur, pyramid_points, top):
+def draw_pyramid(tur, draw=False):
+    topbase = calculate_points(pyramid_pos, 0, pyramid_height//2, pyramid_ver_angles)
+    pyramid_points = calculate_points(topbase[1], pyramid_width, pyramid_width*math.cos(math.radians(pyramid_ver_angles[1])), pyramid_hor_angles)
+    if draw: 
+        screen.tracer(1)
     jump(tur, pyramid_points[0])
     for p in pyramid_points:
         tur.goto(p)
-        tur.goto(pyramid_pos[0], pyramid_pos[1]+top)
+        tur.goto(topbase[0])
         jump(t1, p)
     tur.goto(pyramid_points[0])
+    screen.tracer(0)
 
 def jump(tur, pos):
     tur.pu()
@@ -32,24 +36,23 @@ t1.hideturtle()
 
 pyramid_base_sides = 4
 pyramid_height = 200
-pyramid_pos = [0, -100]
-pyramid_base_angles = [x for x in range(15, 375, 360//pyramid_base_sides)]
+pyramid_width = 100
+pyramid_pos = [0, 0]
+pyramid_hor_angles = [x for x in range(15, 375, 360//pyramid_base_sides)]
+pyramid_ver_angles = [80, 260]
 
-jump(t1, pyramid_pos)
-first_pyramid = ellipse(t1, 100, 25)
-screen.tracer(1)
-draw_pyramid(t1, first_pyramid, pyramid_height)
-sleep(.3)
-screen.tracer(0)
+draw_pyramid(t1, True)
 
 while True:
-    pyramid_base = ellipse(t1, 100, 25)
-    draw_pyramid(t1, pyramid_base, pyramid_height)
-    for i in range(len(pyramid_base_angles)):
-        pyramid_base_angles[i] += 1
-        if pyramid_base_angles[i] >= 360:
-            pyramid_base_angles[i] -= 360
-    jump(t1, pyramid_pos)
+    draw_pyramid(t1)
+    for i in range(len(pyramid_hor_angles)):
+        pyramid_hor_angles[i] += 1
+        if pyramid_hor_angles[i] >= 360:
+            pyramid_hor_angles[i] -= 360
+    for i in range(len(pyramid_ver_angles)):
+        pyramid_ver_angles[i] += 1
+        if pyramid_ver_angles[i] >= 360:
+            pyramid_ver_angles[i] -= 360
     screen.update()
     sleep(.01)
     t1.clear()
