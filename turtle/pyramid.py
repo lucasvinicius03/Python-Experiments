df9693c871dd176673667c231f7f81250a479348
Lucas <89718602+luscra0@users.noreply.github.com>
2021-11-01 02:34:00 -0300

    Ver and Hor axis now referred to as X and Y axis

--- turtle/pyramid.py
+++ turtle/pyramid.py
@@ -11,16 +11,16 @@ def calculate_points(pos, r1, r2, angles):
     return points
 
 def draw_pyramid(tur, draw=False):
-    topbase = calculate_points(pyramid_pos, 0, pyramid_height//2, pyramid_ver_angles)
-    pyramid_points = calculate_points(topbase[1], pyramid_width, pyramid_width*math.cos(math.radians(pyramid_ver_angles[1])), pyramid_hor_angles)
+    y_points = calculate_points(pyramid_pos, 0, pyramid_height//2, pyramid_y_angles)
+    x_points = calculate_points(y_points[1], pyramid_width, pyramid_width*math.cos(math.radians(pyramid_y_angles[1])), pyramid_x_angles)
     if draw: 
         screen.tracer(1)
-    jump(tur, pyramid_points[0])
-    for p in pyramid_points:
+    jump(tur, x_points[0])
+    for p in x_points:
         tur.goto(p)
-        tur.goto(topbase[0])
+        tur.goto(y_points[0])
         jump(t1, p)
-    tur.goto(pyramid_points[0])
+    tur.goto(x_points[0])
     screen.tracer(0)
 
 def jump(tur, pos):
@@ -29,29 +29,32 @@ def jump(tur, pos):
     tur.pd()
 
 screen = turtle.Screen()
-screen.tracer(0)
 t1 = turtle.Turtle()
 t1.hideturtle()
 
 pyramid_base_sides = 4
 pyramid_height = 200
 pyramid_width = 100
+spin_x = True
+spin_y = True
 pyramid_pos = [0, 0]
-pyramid_hor_angles = [x for x in range(15, 375, 360//pyramid_base_sides)]
-pyramid_ver_angles = [80, 260]
+pyramid_x_angles = [x for x in range(15, 375, 360//pyramid_base_sides)]
+pyramid_y_angles = [80, 260]
 
 draw_pyramid(t1, True)
 
 while True:
     draw_pyramid(t1)
-    for i in range(len(pyramid_hor_angles)):
-        pyramid_hor_angles[i] += 1
-        if pyramid_hor_angles[i] >= 360:
-            pyramid_hor_angles[i] -= 360
-    for i in range(len(pyramid_ver_angles)):
-        pyramid_ver_angles[i] += 1
-        if pyramid_ver_angles[i] >= 360:
-            pyramid_ver_angles[i] -= 360
+    if spin_x:
+        for i in range(len(pyramid_x_angles)):
+            pyramid_x_angles[i] += 1
+            if pyramid_x_angles[i] >= 360:
+                pyramid_x_angles[i] -= 360
+    if spin_y:
+        for i in range(len(pyramid_y_angles)):
+            pyramid_y_angles[i] += 1
+            if pyramid_y_angles[i] >= 360:
+                pyramid_y_angles[i] -= 360
     screen.update()
     sleep(.01)
     t1.clear()
